5662dfcdc522086078d6398c6a813ed33b5de8e5
JonathanWekesser <118290713+JonathanWekesser@users.noreply.github.com>
2023-11-17 17:19:47 +0100

    addmoney

--- src/functions/getStuff.js
+++ src/functions/getStuff.js
@@ -8,20 +8,32 @@ app.http('getStuff', {
 
         //requests: name, 
 
-        let name = request.query.get('name') || await request.text();
-        let calculation = request.query.get('calculation') || await request.text();
-        let luckynumber = request.query.get('luckynumber') || await request.text();
-        let favouriteanimal = request.query.get('favouriteanimal') || await request.text();
+        let name = request.query.get('name') || await request.text.name();
+        let calculation = request.query.get('calculation') || await request.text.calculation();
+        let luckynumber = request.query.get('luckynumber') || await request.text.luckynumber();
+        let favouriteanimal = request.query.get('favouriteanimal') || await request.text.favouriteanimal();
+        let addmoney = request.query.get('addmoney') || await request.text.addmoney();
         
         if (name != '' && name != null) {
             return { body: `Wie kann man nur "${name}" heißen ... ?` };
         } else if (calculation != '' && calculation != null) {
             return { body: `Warum fragst du mich, rechne das gefälligst selber aus!` };
         } else if (luckynumber != '' && luckynumber != null) {
-            luckynumber = parseInt(luckynumber);
-            return { body: `Wusstest du dass deine Lucky Number addiert mit 5 "${luckynumber + 5}" ergibt?` };
+            if (!isNaN(luckynumber)) {
+                luckynumber = parseInt(luckynumber);
+                return { body: `Wusstest du dass deine Lucky Number addiert mit 5 "${luckynumber + 5}" ergibt?` };
+            } else {
+                return { body: `Bist du dir bewusst, dass du eine Zahl angeben musst, damit das hier funktionieren kann?` };
+            }
         } else if (favouriteanimal != '' && favouriteanimal != null) {
-            return { body: `Wusstest du, dass "${favouriteanimal}" gebraten ganz gut schmeckt?` };
+            return { body: `Wusstest du, dass "${favouriteanimal}" gebraten wirklich ausgezeichnet schmeckt? Mmmhhh🤌` };
+        } else if (addmoney != '' && addmoney != null) {
+            if (!isNaN(addmoney)) {
+                addmoney = parseInt(addmoney) * 100;
+                return { body: `Vielen Dank, Sie haben hiermit "${addmoney}" € an Nestle gespendet!` };
+            } else {
+                return { body: `Schon gewusst? Buchstaben kann man nicht auf Konten überweisen!` };
+            }
         } else {
             return { body: `Ich weiß wo du wohnst, also gib lieber keine Dummheiten ein.` };
         }
