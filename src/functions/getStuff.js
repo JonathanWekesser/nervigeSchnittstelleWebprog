8d182efba8f7e1e416918bbbf6f24fa73c5f65cb
JonathanWekesser <118290713+JonathanWekesser@users.noreply.github.com>
2023-11-17 16:38:51 +0100

    first commit

---
+++ src/functions/getStuff.js
@@ -0,0 +1,29 @@
+const { app } = require('@azure/functions');
+
+app.http('getStuff', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        //requests: name, 
+
+        let name = request.query.get('name') || await request.text();
+        let calculation = request.query.get('calculation') || await request.text();
+        let luckynumber = request.query.get('luckynumber') || await request.text();
+        let favouriteanimal = request.query.get('animal') || await request.text();
+        
+        if (name != '' && name != null) {
+            return { body: `Wie kann man nur "${name}" heißen ... ?` };
+        } else if (calculation != '' && calculation != null) {
+            return { body: `Warum fragst du mich, rechne das gefälligst selber aus!` };
+        } else if (luckynumber != '' && luckynumber != null) {
+            luckynumber = parseInt(luckynumber);
+            return { body: `Wusstest du dass deine Lucky Number addiert mit 5 "${luckynumber + 5}" ergibt?` };
+        } else if (favouriteanimal != '' && favouriteanimal != null) {
+            return { body: `Wusstest du, dass "${favouriteanimal}" gebraten ganz gut schmeckt?` };
+        } else {
+            return { body: `Ich weiß wo du wohnst, also gib lieber keine Dummheiten ein.` };
+        }
+    }
+});
